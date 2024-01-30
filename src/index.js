d78aa64c887b913ec554a790d4ead35dbee3fa4f
Thang.LuuManh <110220024+thang-luumanh@users.noreply.github.com>
2024-01-30 17:13:03 +0700

    prettier, Lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -2,27 +2,32 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
 const port = 3000;
 const route = require('./routes');
 
 //app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 
 app.use(express.json());
 
 //template engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}));
-app.set('view engine','hbs')
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-})
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,20 @@
 const newsRouter = require('./news.route');
 
-function route(app){
-    app.get('/', (req, res) => {
-        res.render('home')
-      })
+            function route(app) {
+    app.get('/', (req,              res) => {
+        res.render('home');
+    });
 
     app.use('/news', newsRouter);
-    
+
     app.get('/search', (req, res) => {
-        res.render('search')
-    })
-    
+        res.render('search');
+    });
+
     app.post('/search', (req, res) => {
-        console.log(req.body.params)
-        res.send('')
-    })
+        console.log(req.body.params);
+        res.send('');
+    });
 }
 
 module.exports = route;
